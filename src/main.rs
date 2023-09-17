7829ba92635d4cb6336c9a16bb6cdff949ccedb4
vpgle <Werth_Ln@outlook.com>
2023-09-17 13:34:36 +0800

    checknote870_new of src/main.rs upload

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::collections::HashMap;
+#[derive(Debug)]
+struct Datastruct {
+    key: String,
+    a_value: Vec<String>,
+    b_value: HashMap<String, String>,
+    c_value: Vec<String>,
+}
+impl Datastruct {
+    fn average(&self) -> u64 {
+        // 这是计算均值部分
+        // 计算b_value中数据的均值
+        println!("{:?}", &self);
+        999_999_999
+    }
+}
 fn main() {
     dbg!("Hello, world!");
 }
