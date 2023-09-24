f9c4e810e614f890715cec2d39c64bc8a608e635
vpgle <Werth_Ln@outlook.com>
2023-09-24 15:48:37 +0800

    练习命令行提交文件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[warn(dead_code)]
+use std::fmt;
 use std::collections::HashMap;
 #[derive(Debug)]
 struct Datastruct {
@@ -7,13 +9,35 @@ struct Datastruct {
     c_value: Vec<String>,
 }
 impl Datastruct {
-    fn average(&self) -> u64 {
+    fn average(self: &Self) -> u64 {
         // 这是计算均值部分
         // 计算b_value中数据的均值
-        println!("{:?}", &self);
+        println!("{}", self);
+
+        dbg!("{}", &self.b_value);
+
+        let hmap: &HashMap<String, String>= &self.b_value;
+        println!("{:?}", hmap);
+
         999_999_999
     }
 }
+impl fmt::Display for Datastruct {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "(\n    key: {:?},\na_value: {:?},\nb_value: {:?},\nc_value: {:?},\n)",
+               self.key, self.a_value, self.b_value, self.c_value)
+    }
+}
 fn main() {
-    dbg!("Hello, world!");
+    let mut b_value = HashMap::new();
+    b_value.insert( "a".to_string(), "b".to_string());
+    let m = Datastruct {
+        key: "key".to_string(),
+        a_value: vec!["a_value".to_string()],
+        b_value: b_value,
+        c_value: vec!["c_value".to_string()],
+    };
+//  println!("{}", m);
+
+    println!("{:?}", m.average());
 }
